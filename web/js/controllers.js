66b7e0d21c4fc3f7abe111d2acceb8867ff505b0
Gregory Auld <greg@auld.name>
2013-12-07 23:24:58 +0900

    Game stuff

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -7,6 +7,10 @@ var worldMapControllers = angular.module('worldMapControllers', []);
 worldMapControllers.controller('worldMapController', ['$scope', '$http',
   function($scope, $http) {
 
+    var taunts = ["( ≧Д≦)","o(-`д´- ｡) ","((╬ಠิ﹏ಠิ))","(；￣Д￣） ","ಠ_ರೃ ","(」゜ロ゜)」","(;¬_¬) ","凸(｀0´)凸 ","(*｀へ´*)","（；¬＿¬) ","-`д´- ","(*≧m≦*)","(｡+･`ω･´) ","｡゜(｀Д´)゜｡ ","(/ﾟДﾟ)/","(*￣m￣) ","( •̀ω•́ )σ ","(＃｀д´)ﾉ","(>人<) ","( ꒪Д꒪)ノ ","(#ಠQಠ#)","(¬_¬) ","(　ﾟДﾟ)＜!! ","(‡▼益▼)","(¬､¬) ","( ಠ ಠ ) ","(´･益･｀*)","(¬▂¬) ","(･｀ｪ´･)つ ","(´Д｀)","(⋋▂⋌) ","(,,#ﾟДﾟ) ","(҂⌣̀_⌣́)","＼(｀0´)／ ","lol what are you staring at this for?","(； ｀ｪ´ ；)b三b ","(>_<)","ヽ(｀⌒´メ)ノ ","(；¬д¬) ","（＞д＜）","ヽ(●-`Д´-)ノ ","（;≧皿≦） ","(¬д¬。)","ヽ༼ ಠ益ಠ ༽ﾉ ","(((p(>o<)q))) ","（≧▼≦；)","(≧σ≦) ","(◣_◢) ","(ᇂ∀ᇂ╬)","(╬ﾟ◥益◤ﾟ) ","(ू˃̣̣̣̣̣̣︿˂̣̣̣̣̣̣ ू) ","(ノ≧┏Д┓≦)ノ","hurry up","(`ヘ´)","（´・ω・｀）","（ ´Д｀）","（　ﾟДﾟ）"," ┐('～`；)┌ ","（´∀｀）","（　´_ゝ`）"," Σ(゜д゜;)","(*´Д`)","(─▽─)","(ﾟ∀ﾟ)"],
+        earthHex = '#E3ECCE',
+        seaHex = '#BBDDFF';
+
     function random_color(format, saturation, lightness){
       var rint = Math.round(0xffffff * Math.random()),
           sat = saturation || "100%",
@@ -41,6 +45,74 @@ worldMapControllers.controller('worldMapController', ['$scope', '$http',
       setTimeout(function() {fn.call(null, data);}, timeout);
     }
 
+    //+ Jonas Raoni Soares Silva
+    //@ http://jsfromhell.com/array/shuffle [v1.0]
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+
+    function getShapeIdForCountryName(i){
+        return i.replace(/\s+/g, '');
+    }
+
+    function doAWin(name) {
+        var d3Delay = 500,
+            msgDelay = 1000,
+            guy,
+            message = $scope.win.replace("{{name}}", name);
+
+        // TODO: pause taunting
+
+        // Increment score by one
+        $scope.score++;
+
+        // Change guyName to the winning message
+        $scope.guyName = message;
+
+        // Add winMsg class to guyName and score div, remove it a second later
+        $scope.guyMsgAnimClass = 'winMsg';
+        $scope.scoreMsgAnimClass = 'winMsg';
+        MakeTimeoutCall(function(n){
+            $scope.$apply(function() {
+                $scope.guyMsgAnimClass = '';
+                $scope.scoreMsgAnimClass = '';
+            });
+        }, null, msgDelay);
+
+        // Fill the country shape
+        guy = document.getElementById(getShapeIdForCountryName(name));
+
+        d3.select(guy).transition().style("fill", random_color("hsl"));          
+        d3.select(guy).transition().delay(d3Delay).style("fill", earthHex);
+
+        // Reset the form
+        $scope.answer = "";
+
+        // TODO: resume taunting
+    }
+
+    function doALose(name) {
+        var msgDelay = 1000,
+            guy,
+            message = $scope.lose.replace("{{name}}", name);
+
+        // TODO: pause taunting
+
+        // Change guyName to the losing message
+        $scope.guyName = message;
+
+        // Add loseMsg class to guyName, remove it a second later
+        $scope.guyMsgAnimClass = 'loseMsg';
+        MakeTimeoutCall(function(n){
+            $scope.$apply(function() {
+                $scope.guyMsgAnimClass = '';
+            });
+        }, null, msgDelay);
+
+        // TODO: resume taunting
+    }
+
     var width = 960,
         height = 960,
         guy = "",
@@ -55,12 +127,15 @@ worldMapControllers.controller('worldMapController', ['$scope', '$http',
           .attr("width", width)
           .attr("height", height);
 
-    var taunts = ["( ≧Д≦) ","o(-`д´- ｡) ","((╬ಠิ﹏ಠิ))","(；￣Д￣） ","ಠ_ರೃ ","(」゜ロ゜)」","(;¬_¬) ","凸(｀0´)凸 ","(*｀へ´*)","（；¬＿¬) ","-`д´- ","(*≧m≦*)","(｡+･`ω･´) ","｡゜(｀Д´)゜｡ ","(/ﾟДﾟ)/","(*￣m￣) ","( •̀ω•́ )σ ","(＃｀д´)ﾉ","(>人<) ","( ꒪Д꒪)ノ ","(#ಠQಠ#)","(¬_¬) ","(　ﾟДﾟ)＜!! ","(‡▼益▼)","(¬､¬) ","( ಠ ಠ ) ","(´･益･｀*)","(¬▂¬) ","(･｀ｪ´･)つ ","(´Д｀)","(⋋▂⋌) ","(,,#ﾟДﾟ) ","(҂⌣̀_⌣́)","＼(｀0´)／ ","(； ｀ｪ´ ；)b三b ","(>_<)","ヽ(｀⌒´メ)ノ ","(；¬д¬) ","（＞д＜）","ヽ(●-`Д´-)ノ ","（;≧皿≦） ","(¬д¬。)","ヽ༼ ಠ益ಠ ༽ﾉ ","(((p(>o<)q))) ","（≧▼≦；)","(≧σ≦) ","(◣_◢) ","(ᇂ∀ᇂ╬)","(╬ﾟ◥益◤ﾟ) ","(ू˃̣̣̣̣̣̣︿˂̣̣̣̣̣̣ ू) ","(ノ≧┏Д┓≦)ノ","hurry up","(`ヘ´)","（´・ω・｀）","（ ´Д｀）","（　ﾟДﾟ）","why no text lol"," ┐('～`；)┌ ","（´∀｀）","（　´_ゝ`）"," Σ(゜д゜;)","(*´Д`)","(─▽─)","(ﾟ∀ﾟ)"];
+    $scope.win = "(*ﾟ▽ﾟ)/ﾟ･:*【{{name}}】*:･ﾟ＼(ﾟ▽ﾟ*)";
+    $scope.lose = "{{name}}? ヽ(〃' x ' )ﾉ彡☆ no";
 
-    // Used in the template
+    $scope.taunts = taunts;
+
     $scope.guyName = "Go!!!!!!!";
-    $scope.placeholder = "typing typing typing typing typing typing typing";
-    $scope.currentScore = 0;
+    $scope.score = 0;
+
+    $scope.names = [], $scope.lowercaseNames = []; // These are later pushed to with the geo data file data
 
     svg.append("path")
         .datum(graticule)
@@ -69,55 +144,51 @@ worldMapControllers.controller('worldMapController', ['$scope', '$http',
 
     $http.get('data/gistfile1.json').success(function(world) {
         console.log("have world object");
-        console.log(world);
         
-        var features = world.features;
+        var features = world.features,
+            taunts = shuffle($scope.taunts);
+
+        $scope.total = features.length;
+
+        // Construct an array of country names
+        for (x=0; x< features.length; x++){
+            $scope.names.push(features[x].properties.name);
+            $scope.lowercaseNames.push(features[x].properties.name.toLowerCase()); // for ease of searching
+        }
+
+        $scope.handleAnswer = function(answer) {
+            var lowercaseInput = answer.toLowerCase(),
+                indexOf = $scope.lowercaseNames.indexOf(lowercaseInput);
+
+            if (indexOf === -1) {
+                doALose(answer);
+            } else {
+                doAWin($scope.names[indexOf]);
+            }
+        }
 
         // Draw the initial map
         svg.selectAll('path')
             .data(world.features)
             .enter().append('path')
             .attr('d', d3.geo.path().projection(projection))
-            .attr('id', function(d){return d.properties.name.replace(/\s+/g, '')})
-            .style('fill', 'gray')
-            .style('stroke', 'white')
-            .style('stroke-width', 1);
+            .attr('id', function(d){return getShapeIdForCountryName(d.properties.name);})
+            .style('fill', '#BBDDFF')
+            .style('stroke', 'gray')
+            .style('stroke-width', 0.5);
 
+        // Taunts
         for (x=0; x < taunts.length; x++){
-          var delay = 1000 * x,
-              taunt = taunts[x];
+            var delay = 1000 * x,
+                taunt = taunts[x];
 
-          MakeTimeoutCall(function(n) {
-              $scope.$apply(function() {
-                $scope.guyName = n;
-              });
-          }, taunt, delay);
+            MakeTimeoutCall(function(n) {
+                $scope.$apply(function() {
+                    $scope.guyName = n;
+                });
+            }, taunt, delay);
         }
 
-        // Set timed functions for coloring the map and showing the country name
-        for (x=0; x< features.length; x++){
-          var feature = features[x],
-              delay = 200 * x,
-              name = feature.properties.name,
-              guy;
-
-          guy = document.getElementById(name.replace(/\s+/g, ''));
-
-          /*
-          d3.select(guy).transition().delay(delay)
-             .style("fill", random_color("hsl"));
-
-          MakeTimeoutCall(function(n){
-              // This callback is later run outside of Angular -- while it has access
-              // to $scope.guyName, setting it normally doesn't trigger Angular's data bindings.
-              // Run inside $scope.$apply to get around this
-              $scope.$apply(function() {
-                $scope.guyName = splitCamel(n);
-              });
-          }, name, delay);
-          */
-
-        }
     });
     
     d3.select(self.frameElement).style("height", height + "px");
